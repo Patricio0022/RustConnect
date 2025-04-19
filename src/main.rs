d7b9a9c0230d485a7dfa2f0717b184e31f10878d
patricio0022 <niellucsx@hotmail.com>
2025-04-19 17:44:53 -0300

    first

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::net::TcpListener;
+
+fn main() {
+    struct Server {
+        url: String,
+        port: i32,
+    }
+
+    let server = Server {
+        url: "localhost".to_string(),
+        port: 8080,
+    };
+
+    // // bind the server to the specified address // associa
+    let listener = TcpListener::bind(format!("{}:{}", server.url, server.port)).unwrap(); // return the server or panic if it fails
+    println!("{}", listener.local_addr().unwrap()); // return the local socket address of the server
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("Connection received");
+
+                let stream_clone = stream.try_clone().unwrap();
+                let reader = BufReader::new(stream);
+                let mut writer = BufWriter::new(stream_clone);
+
+                for i in reader.lines(){
+                    let msg = "Pong";
+                    println!("{:?}", i);
+                    writer.write_all(msg.as_bytes()).err();
+                    writer.flush().unwrap();
+                }
+
+            }
+            Err(e) => {
+                println!("Error accepting connection: {}", e);
+
+            }
+        }
+    }
+
+}
